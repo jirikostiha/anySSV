ec81cdd2869a89437ffc6d0d13cffc95598ec9c2
Jiri Kostiha <ijkdata@gmail.com>
2020-04-01 13:35:47 +0200

    add import command

---
+++ src/AnySsv.Cli.PowerShell/ImportSsvCommand.cs
@@ -0,0 +1,29 @@
+﻿namespace Wayout.Ssv.Cli.PowerShell
+{
+    using System.Management.Automation;
+
+    //https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/import-csv?view=powershell-6  
+
+    [Cmdlet(VerbsData.Import, "Ssv")]
+    public class ImportSsvCommand : PSCmdlet
+    {
+        [Parameter(Mandatory = true)]
+        [Alias("lp")]
+        [ValidateNotNullOrEmpty]
+        public string LiteralPath { get; set; }
+
+        [Parameter()]
+        [Alias("d")]
+        public string Delimiter { get; set; } = ";";
+
+        [Parameter()]
+        [Alias("cn")]
+        public string[] ColumnNames { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            var ssv = Ssv.Load(LiteralPath, Delimiter);
+            WriteObject(ssv);
+        }
+    }
+}
